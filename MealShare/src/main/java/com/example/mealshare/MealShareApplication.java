af9c9a54685f40dff9611a518f05b81712e729f5
Andrei <zubek.a@northeastern.edu>
2025-02-06 22:52:33 -0500

    Added .gitignore

---
+++ MealShare/src/main/java/com/example/mealshare/MealShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealShareApplication.class, args);
+    }
+
+}
---
+++ MealShare/src/test/java/com/example/mealshare/MealShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
