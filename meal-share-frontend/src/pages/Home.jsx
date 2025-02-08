436620490f29f7af1450cc44c440edf660f6a7fc
Andrei <zubek.a@northeastern.edu>
2025-02-07 20:46:07 -0500

    Day 2

---
+++ meal-share-frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Meal Share!</h1>
+            <p>Discover and share meal plans easily.</p>
+        </div>
+    );
+};
+
+export default Home;
